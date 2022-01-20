e15ad0c955459e93d6f716338573f25f645dc37f
LilThawg <72289126+phamthanh29@users.noreply.github.com>
2022-01-20 14:42:50 +0700

    Delete VCS-training/Ex1 directory

--- VCS-training/Ex1/2/pkg/models/user.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type User struct{
-	Id int `json:"id" gorm:"primaryKey"`
-	Name string `json:"name"`
-	Dob	string `json:"dob"`
-	Gender string `json:"gender"`
-	Email string `json:"email"`
-}
